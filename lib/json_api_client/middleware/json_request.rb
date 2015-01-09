7cba8ff07cd68aa5a07c2a47a61f39f886d5f67d
Jan Bechstein <jan@bechstein.me>
2015-01-09 20:19:21 +0100

    Perform requests with json_api content type

--- lib/json_api_client/middleware/json_request.rb
+++ lib/json_api_client/middleware/json_request.rb
@@ -2,9 +2,9 @@ module JsonApiClient
   module Middleware
     class JsonRequest < Faraday::Middleware
       def call(environment)
-        environment[:request_headers]["Accept"] = "application/json,*/*"
+        environment[:request_headers]['Accept'] = 'application/vnd.api+json'
         uri = environment[:url]
-        uri.path = uri.path + ".json" unless uri.path.match(/\.json$/) 
+        uri.path = uri.path + '.json' unless uri.path.match(/\.json$/)
         @app.call(environment)
       end
     end
